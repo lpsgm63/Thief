8ff2208f83aa6278f159bcbe07d599560a9fe476
lpsgm63 <lpsgm63@gmail.com>
2019-10-24 20:00:39 +0900

    출발지와 내 위치 추가 및 완성!!!!

--- app/src/main/java/com/example/thief/MainActivity.java
+++ app/src/main/java/com/example/thief/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.thief;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -25,62 +29,30 @@ import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
 import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,OnMapReadyCallback {
-    String departure="10:00", arrival="11:00", transform="Success";
     private static final int GRANTED = 101;
     private static final int DENIED = 102;
-    GoogleMap mMap;
+    private MainHandler dialoghandler = new MainHandler();
+    private GoogleMap mMap;
+    private String departure="10:00", arrival="11:00", transform="Success";
+    private TextView  time_d,time_a,success;
+    private int permissionCheck;
+    private double longtitude,latitude;
 
-    public MainHandler dialoghandler = new MainHandler();
-
-    TextView  time_d,time_a,success;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        permissionCheck = ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        longtitude = location.getLongitude();
+        latitude = location.getLatitude();
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        /*
-        LineChart lineChart = (LineChart)findViewById(R.id.lineChart);
-        ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(4f, 0));
-        entries.add(new Entry(8f, 1));
-        entries.add(new Entry(6f, 2));
-        entries.add(new Entry(2f, 3));
-        entries.add(new Entry(18f, 4));
-        entries.add(new Entry(9f, 5));
-        entries.add(new Entry(16f, 6));
-        entries.add(new Entry(5f, 7));
-        entries.add(new Entry(3f, 8));
-        entries.add(new Entry(7f, 10));
-        entries.add(new Entry(9f, 11));
-
-        LineDataSet dataset = new LineDataSet(entries,"#ofCall");
-
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("January");
-        labels.add("February");
-        labels.add("March");
-        labels.add("April");
-        labels.add("May");
-        labels.add("June");
-        labels.add("July");
-        labels.add("August");
-        labels.add("September");
-        labels.add("October");
-        labels.add("November");
-        labels.add("December");
-
-        LineData data = new LineData(dataset);
-
-        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
-        dataset.setDrawCubic(true); //선 둥글게 만들기
-        dataset.setDrawFilled(true); //그래프 밑부분 색칠
-
-        lineChart.setData(data);
-        lineChart.animateY(5000);
-*/
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
             departure = bundle.get("departure").toString();
@@ -98,7 +70,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         call.setOnClickListener(this);
         satisfaction.setOnClickListener(this);
     }
-
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.sad) {
@@ -126,8 +97,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         };
         new TedPermission(this)
                 .setPermissionListener(permissionListener)
-                .setRationaleMessage("고객센터로의 전화를 위한 권한이 필요해요")
+                .setRationaleMessage("고객센터로의 전화와 위치를 표시하기위한 권한이 필요해요")
                 .setPermissions(Manifest.permission.CALL_PHONE)
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
                 .check();
 
     }
@@ -135,16 +107,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng SEOUL = new LatLng(37.56, 126.97);
-        MarkerOptions markerOptions = new MarkerOptions();
+        LatLng  JICOBA = new LatLng(37.441423, 126.786858);
+        LatLng  Postion = new LatLng(latitude,longtitude);
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions1.position(JICOBA);
+        markerOptions2.position(Postion);
 
-        markerOptions.position(SEOUL);
+        markerOptions1.title("지코바치킨");
+        markerOptions1.snippet("신천 1동점");
+        markerOptions2.title("내 위치");
 
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.addMarker(markerOptions1);
+        mMap.addMarker(markerOptions2);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(JICOBA));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
     }
 
     public class MainHandler extends Handler{
@@ -152,7 +129,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void handleMessage(@NonNull Message msg) {
             switch (msg.what){
                 case GRANTED:
-                    startActivity(new Intent("android.intent.action.CALL", Uri.parse("tel:01041921420")));
+                    startActivity(new Intent("android.intent.action.DIAL", Uri.parse("tel:01041921420")));
                     break;
                 case DENIED:
                     finish();
