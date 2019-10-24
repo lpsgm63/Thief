43cf1a4d311b46bd1081f1d51e72058122868a0d
lpsgm63 <lpsgm63@gmail.com>
2019-10-24 16:56:15 +0900

    지도추가

--- app/src/main/java/com/example/thief/MainActivity.java
+++ app/src/main/java/com/example/thief/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.thief;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Intent;
@@ -14,23 +15,32 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
 import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,OnMapReadyCallback {
     String departure="10:00", arrival="11:00", transform="Success";
     private static final int GRANTED = 101;
     private static final int DENIED = 102;
+    GoogleMap mMap;
 
     public MainHandler dialoghandler = new MainHandler();
 
     TextView  time_d,time_a,success;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         /*
         LineChart lineChart = (LineChart)findViewById(R.id.lineChart);
         ArrayList<Entry> entries = new ArrayList<>();
@@ -89,7 +99,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         satisfaction.setOnClickListener(this);
     }
 
-
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.sad) {
@@ -122,6 +131,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .check();
 
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        markerOptions.position(SEOUL);
+
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+    }
+
     public class MainHandler extends Handler{
         @Override
         public void handleMessage(@NonNull Message msg) {
